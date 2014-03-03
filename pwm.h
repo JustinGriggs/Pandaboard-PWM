afa0371a9a503bc03dbc5645a274fc21a8b68634
JustinGriggs <busted240sx@gmail.com>
2014-03-03 17:07:11 -0500

    Update pwm.h
    added Timer prescaler defines.

--- pwm.h
+++ pwm.h
@@ -7,6 +7,19 @@
 #ifndef PWM_H
 #define PWN_H
 
+//pwm prescaler settings
+#define TIMER_PRESC_1_1 -1
+#define TIMER_PRESC_1_2 0
+#define TIMER_PRESC_1_4 1
+#define TIMER_PRESC_1_8 2
+#define TIMER_PRESC_1_16 3
+#define TIMER_PRESC_1_32 4
+#define TIMER_PRESC_1_64 5
+#define TIMER_PRESC_1_128 6
+#define TIMER_PRESC_1_256 7 
+
+
+
 // opaque pointer to timer object
 static struct omap_dm_timer *timer_ptr;
 
